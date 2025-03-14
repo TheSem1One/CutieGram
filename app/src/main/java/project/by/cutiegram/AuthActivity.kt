e7d27a4bd31d51befb9852db9473e697e1cd00c1
TheSem1One <animehub021@gmail.com>
2025-03-14 12:53:22 +0200

    adding base of project

---
+++ app/src/main/java/project/by/cutiegram/AuthActivity.kt
@@ -0,0 +1,14 @@
+package project.by.cutiegram
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth) // Установите XML-файл
+
+    }
+}
